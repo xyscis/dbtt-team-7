9b083c068fb6f64602e5df24784c8d2cd2f46f97
xyscis <xinyan.chua.2022@scis.smu.edu.sg>
2024-03-22 21:53:40 +0800

    css for flight reeassignment tab and remove some css styling from main

--- vue-project/src/components/FlightReassignment.vue
+++ vue-project/src/components/FlightReassignment.vue
@@ -1,197 +1,126 @@
+<script setup>
+import NavBar from "./NavBar.vue";
+</script>
 <template>
   <NavBar></NavBar>
-  <div>
-    <div class="wrapper">
-      <div class="container-fluid">
-        <div class="row">
-          <div class="col chart">
-            <div class="title">
-              Real-time monitoring on the number of flight requests
-            </div>
-            <img
-              v-if="showOriginalImage"
-              src="../assets/barchart.PNG"
-              alt="Original Bar Chart"
-            />
-            <img v-else src="../assets/barchart2.PNG" alt="New Bar Chart" />
-          </div>
-          <div class="col">
-            <div class="table-container">
-              <table border="1">
-                <thead>
-                  <tr>
-                    <th>Flight Number</th>
-                    <th>Origin</th>
-                    <th>Destination</th>
-                    <th>Date</th>
-                    <th>Scheduled Departure</th>
-                    <th>Departure</th>
-                    <th>Scheduled Arrival</th>
-                    <th>Arrival</th>
-                    <th>Flight Status</th>
-                    <th>Aircraft Type</th>
-                    <th>Crew ID (Cabin Crew)</th>
-                    <th>Distance (miles)</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr>
-                    <td>SW1009</td>
-                    <td>SEA</td>
-                    <td>DEN</td>
-                    <td>2024-07-01</td>
-                    <td>2024-07-04</td>
-                    <td>09:15:00</td>
-                    <td>2024-07-07</td>
-                    <td>09:00</td>
-                    <td>Delayed</td>
-                    <td>Boeing 737-700</td>
-                    <td>FA209, FA309</td>
-                    <td>1022</td>
-                  </tr>
-                  <tr>
-                    <td>SW1010</td>
-                    <td>GEG</td>
-                    <td>MDW</td>
-                    <td>2024-09-01</td>
-                    <td>2024-09-02</td>
-                    <td>09:15:00</td>
-                    <td>2024-09-05</td>
-                    <td>09:00</td>
-                    <td>Cancelled</td>
-                    <td>Boeing 737 MAX 8</td>
-                    <td>FA210, FA310</td>
-                    <td>1507</td>
-                  </tr>
-                  <tr>
-                    <td>SW1011</td>
-                    <td>ATL</td>
-                    <td>SFO</td>
-                    <td>2024-05-25</td>
-                    <td>2024-05-27</td>
-                    <td>09:15:00</td>
-                    <td>2024-05-30</td>
-                    <td>09:00</td>
-                    <td>Cancelled</td>
-                    <td>Boeing 737 MAX 7</td>
-                    <td>FA211, FA311</td>
-                    <td>2134</td>
-                  </tr>
-                  <tr>
-                    <td>SW1012</td>
-                    <td>AUS</td>
-                    <td>GEG</td>
-                    <td>2024-02-17</td>
-                    <td>2024-02-19</td>
-                    <td>09:15:00</td>
-                    <td>2024-02-22</td>
-                    <td>09:00</td>
-                    <td>Delayed</td>
-                    <td>Boeing 737-800</td>
-                    <td>FA212, FA312</td>
-                    <td>1600</td>
-                  </tr>
-                  <tr>
-                    <td>SW1013</td>
-                    <td>JFK</td>
-                    <td>TPA</td>
-                    <td>2024-11-26</td>
-                    <td>2024-11-28</td>
-                    <td>09:15:00</td>
-                    <td>2024-11-30</td>
-                    <td>09:00</td>
-                    <td>Delayed</td>
-                    <td>Boeing 737-800</td>
-                    <td>FA213, FA313</td>
-                    <td>1006</td>
-                  </tr>
-                </tbody>
-              </table>
-            </div>
-          </div>
+  <div class="container-fluid">
+    <div class="row">
+   
+      <div class="col chart">
+        <div class="title">
+          Real-time monitoring on the number of flight requests
         </div>
-        <div class="search">
-          <button v-on:click="scheduleFlight">Schedule</button>
-        </div>
+        <img
+          v-if="showOriginalImage"
+          src="../assets/barchart.PNG"
+          alt="Original Bar Chart"
+        />
+        <img v-else src="../assets/barchart2.PNG" alt="New Bar Chart" />
+      </div>
+
+      <!-- Table Column -->
+      <div class="col">
+        <div class="table-responsive">
+          <table border="1">
+            <thead>
+              <tr>
+                <th>Flight Number</th>
+                <th>Origin</th>
+                <th>Destination</th>
+                <th>Date</th>
+                <th>Scheduled Departure</th>
+                <th>Departure</th>
+                <th>Scheduled Arrival</th>
+                <th>Arrival</th>
+                <th>Flight Status</th>
+                <th>Aircraft Type</th>
+                <th>Crew ID (Cabin Crew)</th>
+                <th>Distance (miles)</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>SW1009</td>
+                <td>SEA</td>
+                <td>DEN</td>
+                <td>2024-07-01</td>
+                <td>2024-07-04</td>
+                <td>09:15:00</td>
+                <td>2024-07-07</td>
+                <td>09:00</td>
+                <td>Delayed</td>
+                <td>Boeing 737-700</td>
+                <td>FA209, FA309</td>
+                <td>1022</td>
+              </tr>
+              <tr>
+                <td>SW1010</td>
+                <td>GEG</td>
+                <td>MDW</td>
+                <td>2024-09-01</td>
+                <td>2024-09-02</td>
+                <td>09:15:00</td>
+                <td>2024-09-05</td>
+                <td>09:00</td>
+                <td>Cancelled</td>
+                <td>Boeing 737 MAX 8</td>
+                <td>FA210, FA310</td>
+                <td>1507</td>
+              </tr>
+              <tr>
+                <td>SW1011</td>
+                <td>ATL</td>
+                <td>SFO</td>
+                <td>2024-05-25</td>
+                <td>2024-05-27</td>
+                <td>09:15:00</td>
+                <td>2024-05-30</td>
+                <td>09:00</td>
+                <td>Cancelled</td>
+                <td>Boeing 737 MAX 7</td>
+                <td>FA211, FA311</td>
+                <td>2134</td>
+              </tr>
+              <tr>
+                <td>SW1012</td>
+                <td>AUS</td>
+                <td>GEG</td>
+                <td>2024-02-17</td>
+                <td>2024-02-19</td>
+                <td>09:15:00</td>
+                <td>2024-02-22</td>
+                <td>09:00</td>
+                <td>Delayed</td>
+                <td>Boeing 737-800</td>
+                <td>FA212, FA312</td>
+                <td>1600</td>
+              </tr>
+              <tr>
+                <td>SW1013</td>
+                <td>JFK</td>
+                <td>TPA</td>
+                <td>2024-11-26</td>
+                <td>2024-11-28</td>
+                <td>09:15:00</td>
+                <td>2024-11-30</td>
+                <td>09:00</td>
+                <td>Delayed</td>
+                <td>Boeing 737-800</td>
+                <td>FA213, FA313</td>
+                <td>1006</td>
+              </tr>
+            </tbody>
+          </table>
+          </div>
+        
       </div>
     </div>
-  </div>
-  <div v-if="flightsAssigned" class="popup">
-    <div class="popup-content">
-      <p>Flights have been assigned successfully!</p>
-      <button @click="closePopup">Close</button>
-      <h4>Available Flights</h4>
-      <table border="1">
-        <thead>
-          <tr>
-            <th>Select</th>
-            <th>Origin</th>
-            <th>Destination</th>
-            <th>Departure</th>
-            <th>Aircraft Availability</th>
-            <th>Crew Availability</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>
-              <input type="checkbox" v-model="selectedFlights['SEA-DEN']" />
-            </td>
-            <td>SEA</td>
-            <td>DEN</td>
-            <td>09:20:00</td>
-            <td>Yes</td>
-            <td>Yes</td>
-          </tr>
-          <tr>
-            <td>
-              <input type="checkbox" v-model="selectedFlights['GEG-MDW']" />
-            </td>
-            <td>GEG</td>
-            <td>MDW</td>
-            <td>09:20:00</td>
-            <td>Yes</td>
-            <td>Yes</td>
-          </tr>
-          <tr>
-            <td>
-              <input type="checkbox" v-model="selectedFlights['ATL-SFO']" />
-            </td>
-            <td>ATL</td>
-            <td>SFO</td>
-            <td>09:20:00</td>
-            <td>Yes</td>
-            <td>Yes</td>
-          </tr>
-          <tr>
-            <td>
-              <input type="checkbox" v-model="selectedFlights['AUS-GEG']" />
-            </td>
-            <td>AUS</td>
-            <td>GEG</td>
-            <td>09:20:00</td>
-            <td>Yes</td>
-            <td>Yes</td>
-          </tr>
-          <tr>
-            <td>
-              <input type="checkbox" v-model="selectedFlights['JFK-TPA']" />
-            </td>
-            <td>JFK</td>
-            <td>TPA</td>
-            <td>09:20:00</td>
-            <td>Yes</td>
-            <td>Yes</td>
-          </tr>
-        </tbody>
-      </table>
-      <button @click="closePopup">Send</button>
-    </div>
-  </div>
-  <div v-if="flightsScheduled" class="popup">
-    <div class="popup-content">
-      <p>Flights have been successfully assigned !</p>
-      <button @click="closeMsg">Close</button>
+    <div class="row text-center">
+    
+        <div class="search">
+          <button v-on:click="scheduleFlight">Schedule</button>
+   
+      </div>
     </div>
   </div>
 </template>
@@ -206,15 +135,14 @@ export default {
 
   methods: {
     scheduleFlight() {
-      // this.flightsAssigned = true;
       this.$router.push({ path: "/CrewList" });
     },
-
   },
 };
 </script>
 
 <style scoped>
+
 .title {
   text-align: center;
   font-size: 30px;
@@ -222,18 +150,15 @@ export default {
   color: #333; /* Darker font color for better readability */
   font-weight: bold; /* Make title font bolder */
 }
-
-.container-fluid {
-  max-width: 90%; /* Restrict maximum width to avoid overly wide table */
-  margin: auto; /* Center the container */
-  padding: 20px;
-
-  border-radius: 10px; /* Rounded corners */
-  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1); /* Soft shadow around the container */
+.chart img {
+ max-width: 100%; /*Ensure the image does not exceed its container  */
+height: auto; /* Maintain aspect ratio */
+  display: block; /* Remove any default inline styling */
+  margin: 0 auto;/*  Center the image within its container  */
 }
 
 table {
-  width: 100%;
+  /* width: 100%; */
   background: white;
   border-collapse: collapse; /* Remove gaps between table cells */
   margin-top: 20px; /* Space between title and table */
